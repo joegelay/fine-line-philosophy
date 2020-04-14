ced31b6030129b5d7715c677ccea76726bc3bdc8
joegelay <joegelay@gmail.com>
2020-04-14 11:27:05 -0600

    add create action to entries controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -8,4 +8,14 @@ class EntriesController < ApplicationController
         @entry = Entry.find(params[:id])
         render json: @entry, include: [:user, :idea]
     end 
+
+    def create 
+        idea = Idea.find_by(id: params[:idea])
+        user = User.find_by(id: params[:user])
+        @entry = Entry.create(
+            content: params[:content],
+            user: user,
+            idea: idea
+        )
+    end 
 end
