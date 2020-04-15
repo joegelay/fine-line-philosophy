052ff3b870867e772783298056507b29f08c39ab
joegelay <joegelay@gmail.com>
2020-04-15 14:03:49 -0600

    entries create authentication inherits from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+    def authenticate 
+        auth_header = request.headers['Authorization']
+        token = auth_header.split(' ')[1]
+        secret = Rails.application.secret_key_base
+        payload = JWT.decode(token, secret)[0]
+        user_id = payload["user_id"]
+    
+        @user = User.find_by(id: user_id)
+    
+        if !@user
+            render status: :unauthorized
+        end 
+    end 
 end
--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -10,25 +10,11 @@ class EntriesController < ApplicationController
     end 
 
     def create 
-        auth_header = request.headers['Authorization']
-        token = auth_header.split(' ')[1]
-        secret = Rails.application.secret_key_base
-        payload = JWT.decode(token, secret)[0]
-        user_id = payload["user_id"]
-
+        authenticate 
 
         idea = Idea.find_by(id: params[:idea])
-        @user = User.find_by(id: user_id)
-
-        if !@user
-            render status: :unauthorized
-        else 
-            @entry = Entry.create(
-                content: params[:content],
-                user: @user,
-                idea: idea
-            )
-        end 
+       
+        @entry = Entry.create(content: params[:content], user: @user, idea: idea)
     end
 
     # def create 
